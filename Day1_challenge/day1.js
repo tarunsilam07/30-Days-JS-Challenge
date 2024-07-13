ed4a2d4b9c7d89021c67ca8683e0c10d40dcefe9
tarunsilam07 <tarun79767@gmail.com>
2024-07-13 17:42:08 +0000

    Completed the Day1 JS challenge

---
+++ Day1_challenge/day1.js
@@ -0,0 +1,54 @@
+// TASK 1
+var a=37  //Declared a variable using var keyword and assigned a number to it
+console.log(a) // logged the value to the console
+
+// TASK 2
+let b="JavaScript" // Declared a variable using let keyword and assigned a string to it 
+console.log(b) // logged the value to the console
+
+//TASK 3
+const isvalid=true //Declared a variable using const keyword and assigned a boolean value to it
+console.log(isvalid) // logged the value to the console
+
+//TASK 4
+//INT DATATYPE
+const num=37
+console.log(typeof num);
+
+//String Datatype
+const user="Tarun"
+console.log(typeof user);
+
+//Boolen Datatype
+const valid=false
+console.log(typeof valid);
+
+//Object
+my_object={
+    user:"tarun",
+    id:52,
+    password:"tarun123"
+}
+console.log(typeof my_object);
+
+
+// array
+const my_array=["Thor","Captain","IronMan"]
+console.log(typeof my_array);  // Arrays are a special type of objects. The typeof operator in JavaScript returns "object" for arrays.
+
+//TASK 5
+//assigning value to a let variable
+let first_val=37
+console.log(first_val);
+//reassigning
+first_val=52
+console.log(first_val);
+
+//TASK 6
+//assigning value to const
+const constant=37
+console.log(constant);
+//reassigning value to const
+//constant=52 --> ERROR :- Assignment to constant variable
+
+
