6de139f739f71fc038afb49b8a5084524beecd67
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 11:04:13 +0000

    Completed the activity3 of Day3 Challenge

--- Day3_challenge/Activity3.js
+++ Day3_challenge/Activity3.js
@@ -28,3 +28,31 @@ switch(day){
         name="Invalid day"
 }
 console.log(name);
+
+//Task 5
+
+let marks=1
+let Grade;
+switch(marks){
+    case 1:
+        Grade="A"
+        break;
+    case 2:
+        Grade="B"
+        break;
+    case 3:
+        Grade="C"
+        break;
+    case 4:
+        Grade="D"
+        break;
+    case 5:
+        Grade="E"
+        break;
+    case 6:
+        Grade="F"
+        break;
+    default:
+        Grade="Invalid"
+}
+console.log(Grade);
