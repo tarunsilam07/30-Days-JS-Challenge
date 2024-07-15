d7fdafcc43721e7f591054cd3efdda9789fac6d1
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 09:58:39 +0000

    Completed the activity3 of Day2 Challenge

---
+++ Day2_challenge/Activity3.js
@@ -0,0 +1,20 @@
+//Task 8
+let a=37
+let b=52
+
+console.log(`Is ${a} Greaterthan ${b}? : `,a>b);
+console.log(`Is ${a} Lessthan ${b}? : `,a<b);
+
+//Task 9
+console.log(`Is ${a} Greaterthan or Equal to ${b}? : `,a>=b);
+console.log(`Is ${a} Lessthan or Equal to ${b}? : `,a<=b);
+
+//Task 10
+let num1=100
+let num2=100
+let num3='100'
+// ==
+console.log("Is num1 equal to num2? ",num1==num2);
+console.log("Is num2 equal to num3? ",num2==num3);
+//===
+console.log("Is num1 equal to num3 in value and in datatype too? ",num1===num3);
