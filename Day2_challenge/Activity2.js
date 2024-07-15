812ced802eca4fd1f71296eb772dddd41a8358cd
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 09:46:44 +0000

    Completed the activity2 of Day2 Challenge

---
+++ Day2_challenge/Activity2.js
@@ -0,0 +1,13 @@
+//Task 6
+let intial=100
+console.log("intial value: ",intial);
+
+intial+=37
+console.log("Value after Incrementation: ",intial);
+
+//Task 7
+intial=100
+console.log("intial value: ",intial);
+
+intial-=37
+console.log("Value after Decrementation: ",intial);
