f2e09cb61b3c7f563705ed2ed0bdd4dfcd59221d
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 10:04:27 +0000

    Completed the activity4 of Day2 Challenge

---
+++ Day2_challenge/Activity4.js
@@ -0,0 +1,16 @@
+let a=37
+let b=52
+let c=9
+let d=7
+
+//Task 11
+let isvalid= (b>a)&&(d>c)
+console.log(isvalid);
+
+//Task 12
+isvalid=(b>a)||(d>c)
+console.log(isvalid);
+
+//Task 13
+isvalid=!isvalid
+console.log(isvalid);
