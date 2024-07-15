99d2f6b161e4ed2f4281422127ede7f41d5a0a79
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 10:43:36 +0000

    Completed the activity2 of Day3 Challenge

---
+++ Day3_challenge/Activity2.js
@@ -0,0 +1,21 @@
+//Task 3
+const a=7
+const b=2
+const c=1
+
+if(a>b){
+    if(a>c)
+        console.log("A is the Largest");
+    else 
+        console.log("C is Largest");
+
+}
+else if(b>a){
+    if(b>c)
+        console.log("B is the Largest");
+    else 
+        console.log("C is the Largest");
+
+}
+else 
+    console.log("There is no larger number");
