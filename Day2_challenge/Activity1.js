6f6fadc77f32b11a46278721bfad16d2e09aa6c9
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 09:41:59 +0000

    Completed the activity1 of Day2 Challenge

---
+++ Day2_challenge/Activity1.js
@@ -0,0 +1,23 @@
+
+let a=52
+let b=37
+//Task 1
+const sum=a+b
+console.log("the sum is: ",sum);
+
+//Task 2
+const diff=a-b
+console.log("the difference is: ",diff);
+
+//Task 3
+const mul=a*b
+console.log("the product is: ",mul);
+
+//Task 4
+const div=a/b
+console.log("the divison is: ",div);
+
+//Task 5
+const rem=a%b
+console.log("the remainder is: ",rem);
+
