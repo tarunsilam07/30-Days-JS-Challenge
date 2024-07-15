ae7b81149f7dbae661c60137cb69191f58defdc7
tarunsilam07 <tarun79767@gmail.com>
2024-07-15 10:29:17 +0000

    Completed the activity1 of Day3 Challenge

---
+++ Day3_challenge/Activity1.js
@@ -0,0 +1,16 @@
+//Task 1 
+let a =-7
+
+if(a<0)
+    console.log("The number is Negative");
+else if(a>0)
+    console.log("The number is Postive");
+else 
+    console.log("The number is Zero");
+
+//Task 2
+let age=20
+if(age<18)
+    console.log("Not Eligible to vote");
+else
+    console.log("Eligible to vote");
