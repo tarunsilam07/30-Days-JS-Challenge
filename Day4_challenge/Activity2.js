12cd158564cb7b80f7933f427bd1d6a89c690b3a
tarunsilam07 <tarun79767@gmail.com>
2024-07-16 05:29:27 +0000

    Completed the Activity2 of Day4 Challenge

--- Day4_challenge/Activity2.js
+++ Day4_challenge/Activity2.js
@@ -1,7 +1,19 @@
 //Task 3
 let sum=0;
 let i=0
-while(i<5)
+while(i<=5){
     sum+=i
+    i++;}
 
 console.log(sum);
+
+//Task 4
+
+let fact=1
+let num=5
+let j=1
+do {
+    fact*=j
+    j++
+} while (j<=num);
+console.log(fact);
