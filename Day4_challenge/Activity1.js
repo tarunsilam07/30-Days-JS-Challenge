95e3eeb37dc0c86ab4bee10aff616fbcf95a1ba3
tarunsilam07 <tarun79767@gmail.com>
2024-07-16 05:06:48 +0000

    Completed the Activity 1 of Day4 Challenge

---
+++ Day4_challenge/Activity1.js
@@ -0,0 +1,9 @@
+//Task 1
+for (let index = 1; index <=10; index++) {
+   console.log(index); 
+}
+
+//Task 2
+for (let index = 1; index <=10; index++) {
+    console.log(`5 X ${index} = ${5*index}`);
+}
