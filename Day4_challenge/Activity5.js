2f26a90b711ee4e19a4ff9bd8f48c60d0b1fe8c1
tarunsilam07 <tarun79767@gmail.com>
2024-07-16 05:52:57 +0000

    Completed the Activity5 of Day4 Challenge

---
+++ Day4_challenge/Activity5.js
@@ -0,0 +1,16 @@
+//Task 8
+for(let i=1;i<=10;i++)
+{
+    if(i==5)
+    {
+        continue;
+    }
+    console.log(i);
+}
+
+//Task 9
+for(let i=1;i<=10;i++){
+    console.log(i);
+    if(i==7)
+        break;
+}
