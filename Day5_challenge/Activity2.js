0c5c7c81fd09c0cc44404e0fecb99ed8c8c7a315
tarunsilam07 <tarun79767@gmail.com>
2024-07-17 05:29:52 +0000

    Completed the Activity2 of Day5 challenge

---
+++ Day5_challenge/Activity2.js
@@ -0,0 +1,20 @@
+//Task 3
+const maximum = function(num1,num2){
+    if(num1>num2)
+        console.log("num1 is greater than num2");
+    else if(num2>num1)
+        console.log("num2 is greater than num1");
+    else
+        console.log("Both are equal");
+}
+
+maximum(7,9)
+
+//Task 3
+
+const concat= function(str1,str2)
+{
+    return str1+str2;
+}
+
+console.log(concat("Thor ","Odinson"))
