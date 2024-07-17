23ac6998243b577412d07f2ceee2d4916e2de23e
tarunsilam07 <tarun79767@gmail.com>
2024-07-17 05:22:02 +0000

    Completed the Activity1 of Day5 challenge

---
+++ Day5_challenge/Activity1.js
@@ -0,0 +1,19 @@
+//Task 1
+
+function check(num){
+    if(num%2==1)
+        console.log("The number is odd");
+    else 
+        console.log("The number is even");
+}
+
+check(7)
+
+//Task 2
+
+function square(num){
+    return num*num;
+}
+
+let ans=square(7)
+console.log(ans);
