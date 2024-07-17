a5e87a55008c8929c48559059adc54502f4d546d
tarunsilam07 <tarun79767@gmail.com>
2024-07-17 05:37:52 +0000

    Completed the Activity3 of Day5 challenge

---
+++ Day5_challenge/Activity3.js
@@ -0,0 +1,17 @@
+//Task 5
+
+const sum=(num1,num2)=>{
+    return num1+num2
+}
+
+console.log(sum(7,92));
+
+//Task 6
+
+const check=(str,char)=>{
+    if(str.includes(char))
+        return true;
+    else 
+        return false
+}
+console.log(check("Dhoni",'D'));
