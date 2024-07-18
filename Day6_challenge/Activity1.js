6c03108e96bb5dec03deefd0a83158f47c909faf
tarunsilam07 <tarun79767@gmail.com>
2024-07-18 09:41:17 +0000

    Completed the Activity1 of Day6 challenge

---
+++ Day6_challenge/Activity1.js
@@ -0,0 +1,7 @@
+//Task 1
+my_arr=[1,2,3,4,5]
+console.log(my_arr);
+
+//Task 2
+console.log(`The first element of the array is ${my_arr[0]}`);
+console.log(`The last element of the array is ${my_arr[my_arr.length-1]}`);
