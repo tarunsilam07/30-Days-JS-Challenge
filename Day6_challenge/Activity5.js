5a7c9829035d6858e4d20f8c50b68cd019ae3427
tarunsilam07 <tarun79767@gmail.com>
2024-07-18 10:17:36 +0000

    Completed the Activity5 of Day 6 challenge

---
+++ Day6_challenge/Activity5.js
@@ -0,0 +1,17 @@
+//Task 12
+let my_arr=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+];
+for(let i=0;i<my_arr.length;i++){
+    for(let j=0;j<my_arr[i].length;j++){
+        console.log(my_arr[i][j]);
+}
+console.log("\n");
+}
+
+//Task 13
+
+console.log(my_arr[1][2]);
