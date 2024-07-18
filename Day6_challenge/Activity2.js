7e33b4e35d5c2a168dc1e7e59d03423d744ddf56
tarunsilam07 <tarun79767@gmail.com>
2024-07-18 09:46:13 +0000

    Completed the Activity2 of Day 6 challenge

---
+++ Day6_challenge/Activity2.js
@@ -0,0 +1,19 @@
+//Task 3
+
+my_arr=[1,2,3,4,5,6]
+console.log(my_arr);
+my_arr.push(7)
+console.log(my_arr);
+
+// Task 4
+my_arr.pop()
+console.log(my_arr);
+
+//Task 5
+my_arr.shift()
+console.log(my_arr);
+
+//Task 6
+my_arr.unshift(1)
+console.log(my_arr);
+
