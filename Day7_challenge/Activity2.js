a666c3e3b6074e441ec799a88ac29386d8143a08
tarunsilam07 <tarun79767@gmail.com>
2024-07-19 11:15:03 +0000

    Completed the Activity2 of Day7 challenge

---
+++ Day7_challenge/Activity2.js
@@ -0,0 +1,20 @@
+//Task 3
+
+my_book={
+    title:"Rich Dad Poor Dad",
+    author:"Robert",
+    year:1997,
+    print:function(){
+        console.log(`The title of the book is ${my_book.title}`);
+        console.log(`The author of the book is ${my_book.author}`);
+    },
+    update:function(newyear){
+        my_book.year=newyear
+        console.log("The updated year is ",my_book.year);
+    }
+}
+
+my_book.print()
+
+//Task 4
+my_book.update(2004)
