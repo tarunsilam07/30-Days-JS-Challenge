2f183d4e7854334aaa39bbbf806d4e4936c24fbb
tarunsilam07 <tarun79767@gmail.com>
2024-07-19 11:06:52 +0000

    Completed the Activity1 of Day7 challenge

---
+++ Day7_challenge/Activity1.js
@@ -0,0 +1,14 @@
+//Task 1
+
+my_book={
+    title:"Rich Dad Poor Dad",
+    author:"Robert",
+    year:1997
+}
+
+console.log(my_book);
+
+//Task 2
+
+console.log(`The title of the book is ${my_book.title}`);
+console.log(`The author of the book is ${my_book.author}`);
