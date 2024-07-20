51dae3a1b66fbe665e39a5c1b47b7784faa2a941
tarunsilam07 <tarun79767@gmail.com>
2024-07-20 05:37:51 +0000

    Completed the Activity5 of Day8 challenge

---
+++ Day8_challenge/Activity5.js
@@ -0,0 +1,27 @@
+// Task 8
+
+const name="Tarun"
+const age=20
+const email="tarun79767@gmail.com"
+
+const user={
+    name,
+    age,
+    email,
+    islogged(){
+        console.log("The user is logged in");
+    }
+}
+
+console.log(user);
+
+//Task 9
+
+const propname="username"
+let user1={
+    [propname]:"Tarun",
+    age:20,
+    roll:"22MH1A4952"
+}
+console.log(user1);
+
