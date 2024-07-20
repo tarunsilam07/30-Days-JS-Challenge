ad228a7f5ca7bffee67bc3d473e21b3f1d122840
tarunsilam07 <tarun79767@gmail.com>
2024-07-20 05:12:19 +0000

    Completed the Activity2 of Day8 challenge

---
+++ Day8_challenge/Activity2.js
@@ -0,0 +1,18 @@
+//Task 3
+
+let array=["Dhoni","Kohli","Rohit","Raina"]
+
+let [finisher,opener1,opener2,middle]=array
+console.log(opener1,opener2);
+
+// Task 4
+
+let obj={
+    name:"Dhoni",
+    age:43,
+    role:"Finisher"
+}
+
+const {name,age,role}=obj
+console.log(name);
+console.log(role);
