f40217d45d15a1f0950b6d34e2665ea8c5acc8a3
tarunsilam07 <tarun79767@gmail.com>
2024-07-20 05:01:52 +0000

    Completed the Activity1 of Day8 challenge

---
+++ Day8_challenge/Activity1.js
@@ -0,0 +1,17 @@
+//Task 1
+
+let name="Dhoni"
+let age=43
+let profession="Cricket"
+
+const literal=`${name} is the one the greats in the profession of ${profession}. He is still the greatest finisher even at the age of ${age}`
+
+console.log(literal);
+
+//Task 2
+
+const multi=`${name} is 
+still the greatest 
+finisher even at the age of
+${age}`;
+console.log(multi);
