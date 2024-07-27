2a833bff6c6c41c0f7d7ff9297cec78376d38698
tarunsilam07 <tarun79767@gmail.com>
2024-07-27 14:49:32 +0000

    Completed the Activity1 of Day 13 challenge by importing the modules form export module created before

---
+++ Day13_challenge/Activity1b.js
@@ -0,0 +1,11 @@
+// Task 1
+const { add, my_object } = require('./Activity1a');
+
+
+const res = add(7, 5);
+console.log(res); 
+
+//Task 2
+const result = my_object.valid(33);
+console.log(result); 
+
