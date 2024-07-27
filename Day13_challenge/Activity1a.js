6f7563c81e702a44ce89963f68bc4f0e709fa9c9
tarunsilam07 <tarun79767@gmail.com>
2024-07-27 14:48:45 +0000

    Completed the export module for Activity1 of Day 13 challenge

---
+++ Day13_challenge/Activity1a.js
@@ -0,0 +1,22 @@
+// Activity1a.js
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+const my_object = {
+    name: "Tarun",
+    id: 52,
+    email: "tarun79767@gmail.com",
+    valid(roll) {
+        if (roll > 10) {
+            return true;
+        }
+        return false;
+    }
+}
+
+module.exports = {
+    add,
+    my_object
+};
