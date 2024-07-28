3ac0ceb09d34f1bd1cd1bab9a8dfe1c2e556b4a7
tarunsilam07 <tarun79767@gmail.com>
2024-07-28 06:47:04 +0000

    Completed the Activity 3 of Day 13 challenge

---
+++ Day13_challenge/Activity3a.js
@@ -0,0 +1,23 @@
+// Activity3a.js
+// export file
+
+const num=77
+const value=99
+const pie=3.14
+
+const add=(a,b)=>{
+    return a+b
+}
+const sub=()=>{
+    return a-b;
+}
+const product=()=>{
+    return a*b;
+}
+const division=()=>{
+    return a/b;
+}
+
+export{
+    num,value,pie,add,sub,product,division
+}
---
+++ Day13_challenge/Activity3b.js
@@ -0,0 +1,17 @@
+// import file
+// Activity 3b.js
+
+import * as files from './Activity3a'
+
+console.log(files.value);
+console.log(files.pie);
+console.log(files.num);
+
+const addition=files.add(7,9)
+console.log(addition);
+const mul=files.product(7,3)
+console.log(mul);
+const div=files.division(7,3)
+console.log(div);
+const diff=files.sub(9,9)
+console.log(diff);
