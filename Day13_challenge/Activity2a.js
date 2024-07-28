049c36815b27ca73170d76e712230aedffd67b6d
tarunsilam07 <tarun79767@gmail.com>
2024-07-28 05:04:30 +0000

    Completed the Task 3 in Activity 2 of Day 13 challenge

---
+++ Day13_challenge/Activity2a.js
@@ -0,0 +1,23 @@
+
+//Task 3
+//Export file
+
+const square=(num)=>{
+    return num*num;
+}
+
+const mul=(num1,num2)=>{
+    return num1*num2;
+}
+
+my_object={
+    name:"Tarun",
+    id:52,
+    email:"tarun79767@gmail.com"
+}
+
+module.exports={
+    square,
+    mul,
+    my_object
+}
---
+++ Day13_challenge/Activity2aa.js
@@ -0,0 +1,10 @@
+// import file
+const {square,mul,my_object}=require('./Activity2a')
+
+const product=mul(7,3)
+console.log(`The product of the numbers is ${product}`);
+
+const squ=square(7)
+console.log(`The square of the number is ${squ}`);
+const name=my_object.name
+console.log(`Welcome back ${name}`);
