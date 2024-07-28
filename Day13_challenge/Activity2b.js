77ee1d3246e98d73420cc3beda907c6b77c50373
tarunsilam07 <tarun79767@gmail.com>
2024-07-28 05:28:05 +0000

    Completed the Task 4 in Activity2 of Day 13 challenge

---
+++ Day13_challenge/Activity2b.js
@@ -0,0 +1,13 @@
+// Activity2b.js
+const valid = (id) => {
+    if (id > 20) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Export the object as the default export
+export default {
+    valid
+};
---
+++ Day13_challenge/Activity2bb.js
@@ -0,0 +1,8 @@
+// Activity2bb.js
+
+// Importing the default export from the Activity2b module
+import doc from './Activity2b.js';
+
+
+const check = doc.valid(30);
+console.log(check); 
