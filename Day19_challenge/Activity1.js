e2cd42449cebaf11bfb29263dc57bf9bd7d66540
tarunsilam07 <tarun79767@gmail.com>
2024-08-06 06:27:50 +0000

    Completed the Activity 1 of Day 19 challenge

---
+++ Day19_challenge/Activity1.js
@@ -0,0 +1,15 @@
+
+
+// Task 1
+let reg=/Javascript/g;
+let str="Javascript is used in many applications! Javascript is a powerful language in terms of building";
+const exps=str.match(reg);
+console.log(exps);
+
+// Task 2
+
+let digi=/\d/g;
+let line="There have been over 1000 people gathering every 7 hrs since the last 73 days"
+const mat=line.match(digi);
+console.log(mat);
+
